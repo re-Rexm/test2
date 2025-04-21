59e7379efa977215373bc55b691d1ed4ea79f913
re-Rexm <138644835+re-Rexm@users.noreply.github.com>
2025-04-21 00:29:23 -0400

    up

--- src/distance.js
+++ src/distance.js
@@ -1,27 +1,27 @@
 AFRAME.registerComponent("distance-calc", {
   init: function () {
-    this.arrowText = this.el;
-    this.targetEl = null;
-    this.cameraEl = document.querySelector("a-camera");
+    this.arrowText = this.el
+    this.targetEl = null
+    this.cameraEl = document.querySelector("a-camera")
   },
 
   tick: function () {
-    if (!this.targetEl || !this.cameraEl) return;
+    if (!this.targetEl || !this.cameraEl) return
 
-    const targetWorldPos = new THREE.Vector3();
-    this.targetEl.object3D.getWorldPosition(targetWorldPos);
+    const targetWorldPos = new THREE.Vector3()
+    this.targetEl.object3D.getWorldPosition(targetWorldPos)
 
-    const cameraPos = new THREE.Vector3();
-    this.cameraEl.object3D.getWorldPosition(cameraPos);
+    const cameraPos = new THREE.Vector3()
+    this.cameraEl.object3D.getWorldPosition(cameraPos)
 
-    let dist = cameraPos.manhattanDistanceTo(targetWorldPos);
-    dist = dist.toFixed(2);
+    let dist = cameraPos.manhattanDistanceTo(targetWorldPos)
+    dist = dist.toFixed(2)
 
-    var distTxt = `Your event is this way.\n Distance: ${dist}m`;
-    this.arrowText.setAttribute("value", distTxt);
+    const distTxt = `Your event is this way.\n Distance: ${dist}m`
+    this.arrowText.setAttribute("value", distTxt)
   },
 
   setTarget: function (newTarget) {
-    this.targetEl = newTarget;
+    this.targetEl = newTarget
   },
-});
+})
