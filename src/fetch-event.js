cdc84ee74a26f65c14d3e3100095247c05fb61f9
re-Rexm <138644835+re-Rexm@users.noreply.github.com>
2025-04-23 14:09:16 -0400

    Update fetch-event.js

--- src/fetch-event.js
+++ src/fetch-event.js
@@ -45,8 +45,8 @@ export async function getAllEvents() {
     querySnapshot.forEach((doc) => {
       const data = doc.data()
       const eventGeo = {
-        latitude: parseFloat(data.eventGeo.latitude),
-        longitude: parseFloat(data.eventGeo.longitude)
+        latitude: parseFloat(data.eventGeo[0]),
+        longitude: parseFloat(data.eventGeo[1])
       };
       // Put data into an array
       mockEvents.push({
